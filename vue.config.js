025068b8687f15c8f68944f0fe529bb2b0a5fba0
Ellie-0613 <tgb05106@gmail.com>
2022-06-30 02:40:46 +0900

    deploy test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-    transpileDependencies: true,
-});
+module.exports = {
+    publicPath: process.env.MODE_ENV === "production" ? "/vue-corona-app" : "/",
+    outputDir: "docs",
+};
